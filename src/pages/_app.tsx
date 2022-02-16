9b363e7898de888b7c3127cb9c14c3a31e5d0895
FranciscoMaik <chiconunes3081@gmail.com>
2022-02-16 18:07:00 -0300

    chore: adding global style

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import type { AppProps } from 'next/app';
+import { GlobalStyle } from '../../globalStyle';
 
 const MyApp: React.FC<AppProps> = function ({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default MyApp;
