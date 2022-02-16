916b8a04accf87c01113d70d2a60b5c72ba42a17
FranciscoMaik <chiconunes3081@gmail.com>
2022-02-16 18:08:57 -0300

    feat: exporting component

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+import { Title } from './Title';
+import { ContainerHeader } from './ContainerHeader';
+import { CardProject } from './CardProject';
+import { Footer } from './Footer';
+
+export { Title, ContainerHeader, CardProject, Footer };
