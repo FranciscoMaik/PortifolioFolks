af28ed0d1b12c7af1408e714252bc1b7db79bf23
FranciscoMaik <chiconunes3081@gmail.com>
2022-02-16 18:07:31 -0300

    style: adding style home page

---
+++ src/styles/Home/styleHome.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background-color: #ebf1f4;
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  padding: 84px;
+  align-items: center;
+
+  .projects {
+    .cards {
+      max-width: 1098px;
+      display: flex;
+      flex-wrap: wrap;
+      gap: 24px;
+      margin-block: 24px;
+    }
+  }
+`;
