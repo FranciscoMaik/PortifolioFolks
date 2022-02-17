f11d723555fc0fd7d1c019de7578b7878d931b66
FranciscoMaik <chiconunes3081@gmail.com>
2022-02-17 11:06:16 -0300

    feat: adit text

--- src/components/ContainerHeader/index.tsx
+++ src/components/ContainerHeader/index.tsx
@@ -8,11 +8,15 @@ export const ContainerHeader: React.FC = function () {
       <img src="perfil.jpeg" alt="imagem de perfil" />
       <div className="informations">
         <h3>Francisco Maik Fonseca Nunes</h3>
-        <p>kgsdfjhasgdfgasjdgfjhgasdhfjgahjsdgfajsdf</p>
-        <p>kgsdfjhasgdfgasjdgfjhgasdhfjgahjsdgfajsdf</p>
-        <p>kgsdfjhasgdfgasjdgfjhgasdhfjgahjsdgfajsdf</p>
-        <p>kgsdfjhasgdfgasjdgfjhgasdhfjgahjsdgfajsdf</p>
-        <p>kgsdfjhasgdfgasjdgfjhgasdhfjgahjsdgfajsdf</p>
+        <p>
+          Sou desenvolvedor Front-end, tenho conhecimento nas liguagens
+          Javascript e Python. Desenvolvo utilizando ReactJs e NextJs para Web e
+          React Native para mobile. Sou graduando em Sistemas de Informação pela
+          UFPI no qual fui representante do CASI e Empresa Júnior Juá. Foi
+          estagiário na Multiveículos.com lideredando o desenvolvimento do seu
+          sistema de catálogo Web e Mobile. Atualmente trabalho na EWZ Capital
+          como desenvolvedor Front-end.
+        </p>
       </div>
     </Container>
   );
