91037aef319aa977aedc53bbdb4eea81eb04dc8d
FranciscoMaik <chiconunes3081@gmail.com>
2022-05-06 16:04:30 -0300

    feat: create component header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+export const Header: React.FC = function () {
+  return (
+    <Container>
+      <h1>
+        <strong>F. Maik</strong> Fonseca
+      </h1>
+      <h2>franciscomaikf@gmail.com</h2>
+    </Container>
+  );
+};
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 24px;
+
+  h1 {
+    color: #13224a;
+    font-weight: 100;
+    font-size: 64px;
+  }
+
+  h2 {
+    font-weight: 100;
+    color: #808080;
+  }
+`;
