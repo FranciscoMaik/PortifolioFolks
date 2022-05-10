5c038f58034f5040f2135d986a1cb500ae8760db
FranciscoMaik <chiconunes3081@gmail.com>
2022-05-10 15:03:53 -0300

    feat: adding informations

--- src/components/Timeline/index.tsx
+++ src/components/Timeline/index.tsx
@@ -11,6 +11,20 @@ const Container = styled.div`
 
 const items: TimelineItem[] = [
   {
+    title: 'Abr 2017',
+    cardTitle: 'Diretor',
+    cardSubtitle: 'Diretor de Tecnologia da Informação',
+    cardDetailedText:
+      'Neste período fui responsável por coordenar, organizar e auxiliar em projetos voltado para alunos do curso de Sistemas de Informação.',
+  },
+  {
+    title: 'Mar 2019',
+    cardTitle: 'Diretor',
+    cardSubtitle: 'Diretor de Gerência de Projetos',
+    cardDetailedText:
+      'Neste período fui responsável por coordenar, organizar e auxiliar na estruturação de projetos para o campus e empresas da região de Picos-PI.',
+  },
+  {
     title: 'Jan 2021',
     cardTitle: 'Estágio',
     cardSubtitle: 'Estágio obrigatório',
@@ -19,10 +33,10 @@ const items: TimelineItem[] = [
   },
   {
     title: 'Set 2021',
-    cardTitle: 'Estágio',
-    cardSubtitle: 'Estágio obrigatório',
+    cardTitle: 'Desenvolvedor',
+    cardSubtitle: 'Desenvolvedor Front-end',
     cardDetailedText:
-      'Neste período estagiei na empresa Multiveículos.com como Gerente de Projetos e Desenvolvedor mobile. Gerenciando o desenvolvimento web da página de vendas da empresa e desenvolvendo o app relacional com essa site.',
+      'Durante a ocupação deste cargo pude trabalhar com tecnologias como ReactJs, NextJs, Gráficos diversos e ferramentas de estilização como Styled-Components e ChackraUi.',
   },
 ];
 
@@ -36,12 +50,13 @@ export const Timeline: React.FC = function () {
         hideControls
         lineWidth={1}
         timelineCircleDimension={8}
+        itemWidth={400}
         theme={{
-          titleColor: '#575c82',
+          titleColor: '#4a90e2',
           cardBgColor: 'transparent',
           cardForeColor: '#000',
           primary: '#575c82',
-          secondary: '#4E86CA',
+          secondary: 'transparent',
         }}
       />
     </Container>
