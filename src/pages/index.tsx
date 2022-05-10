3576d781dbcf984877d411422020e4147e04b078
FranciscoMaik <chiconunes3081@gmail.com>
2022-05-10 15:04:17 -0300

    feat: adding cards skilss

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ import {
   Footer,
   Header,
   Timeline,
+  CardInformation,
 } from '../components';
 
 import { Container } from '../styles/Home/styleHome';
@@ -24,6 +25,32 @@ const Home: React.FC = function () {
         <div className="portfolio">
           <Title name="Sobre" />
           <ContainerHeader />
+          <div className="information">
+            <div className="skills">
+              <h3>Competências</h3>
+              <div>
+                <CardInformation
+                  years={3}
+                  title="JavaScript e TypesCript"
+                  skills={['NextJs', 'ReactJs', 'React Native', 'Tipagens']}
+                />
+                <CardInformation
+                  years={1}
+                  title="Python"
+                  skills={['Processamento de Imagens e Sons']}
+                />
+              </div>
+            </div>
+            <div className="interests">
+              <h3>Interesses</h3>
+              <CardInformation
+                years={0}
+                activeYears={false}
+                title="JavaScript e TypesCript"
+                skills={['Testes', 'Storybook', 'UX e UI']}
+              />
+            </div>
+          </div>
         </div>
 
         <div className="projects">
@@ -32,18 +59,21 @@ const Home: React.FC = function () {
             <CardProject
               header="Site Tiago Bomfim"
               description="Criação de um site pessoal do Professor Tiago Bomfim."
-              image="sitetiago.png"
               linkProject="https://www.tiagobomfim.com.br/"
             />
 
             <CardProject
               header="Person Life"
               description="Aplicativo para criação e comparação de listas de compras."
-              image="personlife.png"
               linkProject="https://harvest-crane-e27.notion.site/PersonLife-433750f045804f54b0a2f9d413b1744f"
             />
           </div>
         </div>
+
+        <div>
+          <Title name="Estatísticas" />
+        </div>
+
         <div>
           <Title name="Experiência" />
           <Timeline />
