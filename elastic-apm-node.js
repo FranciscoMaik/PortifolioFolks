235f35cc70bf120390cec603046eafa1f25d66c8
FranciscoMaik <chiconunes3081@gmail.com>
2023-04-04 18:30:53 -0300

    feat: adjust imports now

--- elastic-apm-node.js
+++ elastic-apm-node.js
@@ -1,6 +1,6 @@
-require('elastic-apm-node').start({
-  serviceName: 'teste-em-prod-aqui',
+module.exports = {
+  serviceName: 'teste-em-prod',
   serverUrl:
     'https://7e90c033dd2e4c5dadef1a305f18fc7b.apm.us-east-2.aws.elastic-cloud.com:443',
   secretToken: '6tvaQVyhQ2g5eUHDrO',
-});
+};
