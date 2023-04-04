7ef356002bc18c1e89d58fecffc54ca9f5ef8b91
FranciscoMaik <chiconunes3081@gmail.com>
2023-04-04 18:10:12 -0300

    feat: remove utl

--- elastic-apm-node.js
+++ elastic-apm-node.js
@@ -1,5 +1,8 @@
-module.exports = {
-  serviceName: "teste-em-prod",
-  serverUrl: "https://7e90c033dd2e4c5dadef1a305f18fc7b.apm.us-east-2.aws.elastic-cloud.com:443",
-  secretToken: "6tvaQVyhQ2g5eUHDrO"
-}
+module.exports = require("elastic-apm-node").start(
+  {
+    serviceName: "teste-em-prod",
+    serverUrl: "https://7e90c033dd2e4c5dadef1a305f18fc7b.apm.us-east-2.aws.elastic-cloud.com:443",
+    secretToken: "6tvaQVyhQ2g5eUHDrO"
+  }
+)
+
