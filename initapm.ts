b4e27f293ab98dc53db1c9d91adca7959e79d4eb
FranciscoMaik <chiconunes3081@gmail.com>
2023-04-05 10:13:52 -0300

    feat: try other option to config

--- initapm.ts
+++ initapm.ts
@@ -1,6 +1,8 @@
-module.exports = {
-  serviceName: 'teste-em-prod',
+import apm from 'elastic-apm-node';
+
+apm.start({
+  serviceName: 'teste-em-aqui',
   serverUrl:
     'https://7e90c033dd2e4c5dadef1a305f18fc7b.apm.us-east-2.aws.elastic-cloud.com:443',
   secretToken: '6tvaQVyhQ2g5eUHDrO',
-};
+});
