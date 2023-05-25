d91a32645be9437d3138135862992614dff89f07
FranciscoMaik <chiconunes3081@gmail.com>
2023-05-25 10:39:30 -0300

    feat: change file to redirect

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+
+  async rewrites() {
+    return [
+      {
+        "source": "/hub/:path*",
+        "destination": "https://www.ewzhub.com/:path*"
+      }
+    ]
+  }
 }
