6a1dcd3f2bdba136a17ca91045c8d4cebfdd31fd
FranciscoMaik <chiconunes3081@gmail.com>
2023-08-16 08:58:34 -0300

    refactor: remove code unused

--- next.config.js
+++ next.config.js
@@ -1,17 +1,4 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-
-  async rewrites() {
-    return [
-      {
-        source: "/hub/:path*",
-        destination: "https://www.ewzhub.com.br/:path*",
-        basePath:  false,
-        // source: "/hub/:path*",
-        // destination: "https://www.ewzhub.com/:path*",
-        // permanent: false
-      }
-    ]
-  }
 }
